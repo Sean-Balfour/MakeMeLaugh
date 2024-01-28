5ea130d52c1799b41e5605b4e10a8faf988c053f
Parker Campbell <mikasa9ve@gmail.com>
2024-01-28 17:57:20 +0000

    fuck knows

--- Assets/Scripts/Interactables.cs
+++ Assets/Scripts/Interactables.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class Interactables : MonoBehaviour
 {
     public bool isInteractable = true;
+    public bool isInRange = false;
+
+    [SerializeField] private Sprite outline;
+    [SerializeField] private Sprite baseSkin;
+    [SerializeField] private SpriteRenderer spriteRenderer;
 
     public virtual void Interact()
     {
         
     }
+    //public void Awake()
+    //{
+        
+    //}
 
     public virtual void EndInteract()
     {
@@ -20,6 +30,38 @@ public class Interactables : MonoBehaviour
     {
         return gameObject.name;
     }
+    
+    public void SkinChange()
+    {
+        if (this.isInRange)
+        {
+            Debug.Log("a");
+            spriteRenderer.sprite = outline;
+        }
+        else
+        {
+            Debug.Log("b");
+            spriteRenderer.sprite = baseSkin;
+        }
+    }
 
+    public void setInRange(bool inRange)
+    {
+        isInRange = inRange;
+        this.SkinChange();
+    }
+
+    protected virtual void Update()
+    {
+        Player player = FindFirstObjectByType<Player>();
+
+        Debug.Log((player.transform.position - transform.position).magnitude);
+
+        if ((player.transform.position - transform.position).magnitude > 6)
+        {
+
+            setInRange(false);
+        }
+    }
 
 }
